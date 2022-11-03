2597ff513e5ab8954c9f5a51f40c4bc55ac81adf
Tim Neumann <neumantm@fius.informatik.uni-stuttgart.de>
2022-11-03 16:14:17 +0100

    fix(Vector): Copy coordinates in constructor instead of referencing
    This ensures that later modification do not modify the original source.

--- exercise1.py
+++ exercise1.py
@@ -7,7 +7,7 @@ from math import isclose
 
 class Vector:
     def __init__(self, coordinates: List[float]) -> None:
-        self._coordinates = coordinates
+        self._coordinates = coordinates.copy()
 
     def __getitem__(self, i: int) -> float:
         return self._coordinates[i]
